e5dba97c03e3bdbe1af9195c4623f84491ed9463
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2015-01-16 19:15:02 +0000

    [glyf] normalise glyph data to 4 byte boundaries for woff2

--- Lib/fontTools/ttLib/tables/_g_l_y_f.py
+++ Lib/fontTools/ttLib/tables/_g_l_y_f.py
@@ -69,6 +69,11 @@ class table__g_l_y_f(DefaultTable.DefaultTable):
 		for glyphName in self.glyphOrder:
 			glyph = self.glyphs[glyphName]
 			glyphData = glyph.compile(self, recalcBBoxes)
+			if ttFont.flavor == "woff2":
+				# pad glyph data to 4-byte boundary
+				glyphSize = len(glyphData)
+				paddedGlyphSize = (glyphSize + 3) & ~3
+				glyphData += b'\0' * (paddedGlyphSize - glyphSize)
 			locations.append(currentLocation)
 			currentLocation = currentLocation + len(glyphData)
 			dataList.append(glyphData)
