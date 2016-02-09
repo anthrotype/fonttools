0bada8c5361d0485ed4e25af69b8a5bb55701446
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-02-09 15:25:18 +0000

    [sfnt] remove ttc stuff (moved to ttc.py)

--- Lib/fontTools/ttLib/sfnt.py
+++ Lib/fontTools/ttLib/sfnt.py
@@ -26,19 +26,6 @@ log = logging.getLogger(__name__)
 
 # -- sfnt directory helpers and cruft
 
-ttcHeaderFormat = """
-		> # big endian
-		TTCTag:                  4s # "ttcf"
-		Version:                 L  # 0x00010000 or 0x00020000
-		numFonts:                L  # number of fonts
-		# OffsetTable[numFonts]: L  # array with offsets from beginning of file
-		# ulDsigTag:             L  # version 2.0 only
-		# ulDsigLength:          L  # version 2.0 only
-		# ulDsigOffset:          L  # version 2.0 only
-"""
-
-ttcHeaderSize = sstruct.calcsize(ttcHeaderFormat)
-
 sfntDirectoryFormat = """
 		> # big endian
 		sfntVersion:    4s
@@ -125,6 +112,7 @@ class SFNTReader(object):
 			infile.seek(0)
 			if sfntVersion == "ttcf":
 				# return new TTCReader object
+				from fontTools.ttLib.ttc import TTCReader
 				return object.__new__(TTCReader)
 			elif sfntVersion == "wOFF":
 				# return new WOFFReader object
@@ -192,51 +180,6 @@ class SFNTReader(object):
 
 	def close(self):
 		self.file.close()
-
-
-class TTCReader(SFNTReader):
-
-	flavor = "ttc"
-
-	def __init__(self, file, checkChecksums=1, fontNumber=-1):
-		self.file = file
-		self.checkChecksums = checkChecksums
-
-		self._readCollectionHeader()
-		if fontNumber == -1:
-			# read the whole collection
-			raise NotImplementedError
-		else:
-			# read single font from collection
-			self.flavor = None
-			self._seekOffsetTable(fontNumber)
-		self._readDirectory()
-
-	def _readCollectionHeader(self):
-		if self.file.read(4) != b"ttcf":
-			raise TTLibError("Not a Font Collection (bad TTCTag)")
-		self.file.seek(0)
-		ttcHeaderData = self.file.read(ttcHeaderSize)
-		if len(ttcHeaderData) != ttcHeaderSize:
-			TTLibError("Not a Font Collection font (not enough data)")
-		sstruct.unpack(ttcHeaderFormat, ttcHeaderData, self)
-		if not (self.Version == 0x00010000 or self.Version == 0x00020000):
-			raise TTLibError("unrecognized TTC version 0x%08x" % self.Version)
-		offsetTableFormat = ">%dL" % self.numFonts
-		offsetTableSize = struct.calcsize(offsetTableFormat)
-		offsetTableData = self.file.read(offsetTableSize)
-		if len(offsetTableData) != offsetTableSize:
-			raise TTLibError("Not a Font Collection (not enough data)")
-		self.offsetTables = struct.unpack(offsetTableFormat, offsetTableData)
-		if self.Version == 0x00020000:
-			# TODO(anthrotype): use flavorData to store version 2.0 signatures?
-			pass
-
-	def _seekOffsetTable(self, fontNumber):
-		"""Move current position to the offset table of font 'fontNumber'."""
-		if not 0 <= fontNumber < self.numFonts:
-			raise TTLibError("specify a font number between 0 and %d (inclusive)" % (self.numFonts - 1))
-		self.file.seek(self.offsetTables[fontNumber])
 
 
 class SFNTWriter(object):
